92ee2f74497ee3d50ec40160cbe8ec831dd2f9a2
Jett <30197659+JettMonstersGoBoom@users.noreply.github.com>
2023-10-22 10:57:49 -0400

    added computed palette variance

--- RayPCX/pcx_test.c
+++ RayPCX/pcx_test.c
@@ -5,6 +5,9 @@
 #include "raylib.h"
 #include "rlgl.h"
 
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+
 #define RAYLIB_PCX_IMPLEMENTATION
 #include "ray_pcx.h"
 
@@ -52,6 +55,43 @@ Color colr;
         .mipmaps = 1
     };
 
+//  make some variations of the input palette
+    for (int q=0;q<256;q++)
+    {
+        unsigned char r,g,b;
+        r = Palettes[0][(q*3)];
+        g = Palettes[0][(q*3)+1];
+        b = Palettes[0][(q*3)+2];
+        Palettes[1][(q*3)]=b;
+        Palettes[1][(q*3)+1]=g;
+        Palettes[1][(q*3)+2]=r;
+
+        Palettes[2][(q*3)]=g;
+        Palettes[2][(q*3)+1]=r;
+        Palettes[2][(q*3)+2]=b;
+
+        Palettes[3][(q*3)]=r;
+        Palettes[3][(q*3)+1]=r;
+        Palettes[3][(q*3)+2]=r;
+
+        Palettes[4][(q*3)]=r/2;
+        Palettes[4][(q*3)+1]=g/2;
+        Palettes[4][(q*3)+2]=b/2;
+
+        Palettes[5][(q*3)]=MIN(255,r*2);
+        Palettes[5][(q*3)+1]=MIN(255,g*2);
+        Palettes[5][(q*3)+2]=MIN(255,b*2);
+
+        Palettes[6][(q*3)]=g;
+        Palettes[6][(q*3)+1]=g;
+        Palettes[6][(q*3)+2]=b;
+
+        Palettes[7][(q*3)]=b;
+        Palettes[7][(q*3)+1]=MIN(255,g*2);
+        Palettes[7][(q*3)+2]=r;
+    }
+
+
     Shader ShaderIndexed = LoadShaderFromMemory(NULL,indexed_shader);
     int ShaderUniformClut = GetShaderLocation(ShaderIndexed,"clut0");
     Texture2D TexturePalette = LoadTextureFromImage(ImagePalette);
@@ -60,15 +100,20 @@ Color colr;
     UnloadImage(LoadedPCXImage);
     UnloadImage(ImagePalette);
 
+    float t=0.0f;
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
+        t+=GetFrameTime();
+
+        int p = ((int)t)&7;
+
         BeginShaderMode(ShaderIndexed);
         SetShaderValueTexture(ShaderIndexed,ShaderUniformClut,TexturePalette);
-        DrawTexture(TextureImage,0,0,(Color){0,0,0,0});
+        DrawTexture(TextureImage,0,0,(Color){p,0,0,0});
         EndShaderMode();
 
         DrawTextureEx(TexturePalette,(Vector2){0,GetScreenHeight()-(16*8)},0,3,WHITE);
